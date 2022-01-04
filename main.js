0ce835da28f085bd52b04a730378867348e80a06
Leif Hetland <lehetland1@gmail.com>
2022-01-03 22:11:12 -0500

    added a point light, ambient light, and pointlight helper as well as switched the basic mesh material to a standard mesh material

--- main.js
+++ main.js
@@ -24,11 +24,24 @@ const ring = new THREE.RingGeometry( 1, 5, 32 );
 
 // adding a material- like wrapping paper for our objs
 // custom shaders can be written in WebGL
-const material = new THREE.MeshBasicMaterial( { color: 'hotpink', side: THREE.DoubleSide, wireframe: true } );
+const material = new THREE.MeshStandardMaterial( { color: 'hotpink', side: THREE.DoubleSide } );
 
 // Creating a mesh to combine the geometry and the material
 const mesh = new THREE.Mesh( ring, material );
 scene.add( mesh );
+
+// Point light with position
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5,5,5)
+
+// Ambient/ flood light
+const ambientLight = new THREE.AmbientLight(0xffffff);
+
+scene.add(pointLight, ambientLight);
+
+// Light Helper
+const lightHelper = new THREE.PointLightHelper(pointLight);
+scene.add(lightHelper);
 
 // Recursive function that gives us an infinite loop- similar to a game loop
 function animate() {
